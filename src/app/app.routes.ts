178e4842a861b8694235b97dd49687f1f16f92c0
ptrdnory <ptrdnoey2003@gmail.com>
2025-12-03 16:00:59 +0700

    edit navbar+add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { InsureHealthDetails } from './modules/insure-health-details/insure-health-details';
+import { LandingPage } from './modules/landing-page/landing-page';
 
 export const routes: Routes = [
     {
+        
         path: "insure-health-details", component: InsureHealthDetails
+    },
+    {
+        path: "landing-page", component: LandingPage
+
     }
 ];
