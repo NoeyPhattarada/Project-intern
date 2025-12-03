90c906edc30bdef1e584bf2efb181095bdc4a190
FahChanakarn <faa.chanakarn@gmail.com>
2025-12-03 15:24:17 +0700

    add insure-health-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InsureHealthDetails } from './modules/insure-health-details/insure-health-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'insure-health-details',
+        component: InsureHealthDetails,
+    }
+];
---
+++ src/app/modules/insure-health-details/insure-health-details.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../shared/components/navbar/navbar";
+
+@Component({
+  selector: 'app-insure-health-details',
+  standalone: true,
+  imports: [Navbar],
+  templateUrl: './insure-health-details.html',
+  styleUrl: './insure-health-details.css',
+})
+export class InsureHealthDetails {
+
+}
