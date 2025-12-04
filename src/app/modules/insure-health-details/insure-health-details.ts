7c870cfbcae7d597cd80b08f99c187809027e8b3
FahChanakarn <faa.chanakarn@gmail.com>
2025-12-04 17:47:36 +0700

    add details-table

--- src/app/modules/insure-health-details/insure-health-details.ts
+++ src/app/modules/insure-health-details/insure-health-details.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DetailsCard } from './details-card/details-card';
+import { DetailsTable } from './details-table/details-table';
 
 @Component({
   selector: 'app-insure-health-details',
   standalone: true,
-  imports: [DetailsCard],
+  imports: [DetailsCard, DetailsTable],
   templateUrl: './insure-health-details.html',
   styleUrl: './insure-health-details.css',
 })
