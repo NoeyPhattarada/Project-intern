ab97ee8ca0a320d1d052eb8fecfd2828538e9f86
ptrdnory <ptrdnoey2003@gmail.com>
2025-12-07 22:51:29 +0700

    edit footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+    readonly FOOTER = FOOTER;
 
 }
 
 const FOOTER: FooterItem[] = [
   {
     image: 'assets/images/x.svg',
+    link: '/x',
   },
   {
     image: 'assets/images/facebook.svg',
+    link: '/facebook',
   },
   {
     image: 'assets/images/instagram.svg',
-
+    link: '/instagram',
   },
+  {
+    image: 'assets/images/github.svg',
+    link: '/github',
+  },
+
+
 ];
 
 interface FooterItem {
   image: string;
+  link:string;
 }
