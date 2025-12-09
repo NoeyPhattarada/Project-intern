f0c93af4d7f1fc76c322f8a734373651613ad012
FahChanakarn <faa.chanakarn@gmail.com>
2025-12-09 22:26:43 +0700

    add hamburger for navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Menu, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   readonly NAVBAR = NAVBAR;
+  readonly Menu = Menu;
+  readonly X = X;
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
 
 const NAVBAR: NavbarItem[] = [
